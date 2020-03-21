38126f73f34de29096c0298f2f56aacfa3d03626
Elina Eickstaedt <contact@khaleesicodes.com>
2020-03-21 19:18:49 +0100

    Initial commit of API

---
+++ coronachecker_api/api.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+URLS = {1 : 'Testurl'}
+
+class CoronaChecker(Resource):
+    def get(self):
+        return {'score': '50%', 'geo-location': "-122.41942,37.77493"}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("URL")
+        args = parser.parse_args()
+        url_id = int(max(URLS.keys())+1)
+        url_id = '%i' % url_id
+        URLS[url_id] = {"URL": args['Url']}
+
+        return URLS[url_id]
+
+
+api.add_resource(CoronaChecker, '/URL/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
